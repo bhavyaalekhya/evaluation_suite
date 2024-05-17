b7b7fdb7dafee5ab36e1147e5e20c8a8e1eca8a7
Bhavya Alekhya <bhavyaalekhya98@gmail.com>
2024-05-17 14:10:17 -0500

    Update evaluate.py

--- Video-LMMs-Inference/TimeChat/evaluate.py
+++ Video-LMMs-Inference/TimeChat/evaluate.py
@@ -53,7 +53,7 @@ def load_data(args, anno_path, split=None):
         ]
     }
     '''
-    file_path = os.path.join(anno_path, f'{split}.caption_coco_format.json')
+    file_path = os.path.join(anno_path)
     with open(file_path, 'r') as f:
         data = json.load(f)["annotations"]
 
